04cbdabda54dc9446023ec304625cec556d5b485
ciekawypl <77553268+ciekawypl@users.noreply.github.com>
2026-04-09 12:31:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,45 +16,8 @@ const rooms: Map<string, room> = new Map
 
 export default defineConfig({
 	plugins: [
-	// 	webSocketServer({
-	// 	path: "/ws",
-	// 	handlers: {
-	// 		onConnect: (connection) => {
-	// 		},
-
-	// 		onMessage: async (connection, message) => {
-	// 			if (message.type == "host") {
-	// 				const manager = getWebSocketManager()
-	// 				const roomId = randomInt(100000, 999999).toString()
-	// 				const quiz = await db.quiz.findUnique({
-	// 					where: { id: message.data }
-	// 				})
-
-	// 				rooms.set(roomId, { quizId: quiz!.id, host: connection.id, players: [] })
-
-	// 				manager.send(connection.id, { type: "roomId", data: roomId })
-	// 			}
-	// 			if (message.type == "connect") {
-	// 				const manager = getWebSocketManager()
-	// 				const room = rooms.get(message.data)
-	// 				const quiz = await db.quiz.findUnique({
-	// 					where: { id: room?.quizId }
-	// 				})
-
-	// 				manager.send(connection.id, { type: "quizData", data: quiz })
-	// 				room?.players.push(connection.id)
-	// 				manager.send(room!.host, {type: "players", data: room?.players.length})
-	// 			}
-	// 			if (message.type == "disconnect") {
-	// 				const room = rooms.get(message.data)
-					
-	// 			}
-	// 		},
-
-	// 		onDisconnect: (connection) => {
-	// 		},
-	// 	},
-	// }),
-	ws,
-	tailwindcss(), sveltekit()]
+		ws,
+		tailwindcss(), 
+		sveltekit()
+	]
 });
