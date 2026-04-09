5338f395a7b4be934315adc344cbc16495e4691d
ciekawy <vajjperr21@gmail.com>
2026-04-09 12:34:46 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { getWebSocketManager, webSocketServer, type WSMessage } from 'sveltekit-ws';
-import { randomInt } from 'crypto';
-import db from './src/lib/server/db';
 import ws from './src/lib/server/ws';
-
-interface room {
-	quizId: string,
-	host: string,
-	players: string[]
-}
-
-const rooms: Map<string, room> = new Map
 
 export default defineConfig({
 	plugins: [
